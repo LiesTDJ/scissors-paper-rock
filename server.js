15c307a602bca24191d8f822f1ab9ab6ecfca4a1
Lies Tdj <liestidjedam@gmail.com>
2022-02-14 19:59:16 +0100

    Setting of the server.js and index.html files

---
+++ server.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import express from 'express';
+import path, {dirname} from'path';
+import {fileURLToPath} from 'url';
+
+
+const app = express();
+const port = 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(
+        'index.html',
+        {
+            root: path.join(__dirname, 'public')
+        },
+        (err) => {
+            if (err) {
+                console.log('Erreur from the get "/" : ', err);
+            }
+        }
+    );
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(
+        '404-error.jpg',
+        {
+            root: path.join(__dirname, 'public/img')
+        },
+        (err) => {
+            if (err) {
+                console.log('Erreur from the 404 : ', err);
+            }
+        }
+    );
+})
+
+app.listen(port, () => {
+    console.log('Running on port : ' + port);
+})
