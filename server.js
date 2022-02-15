93549c348ac39328da7248b4c4bb53b1bad7d715
Lies Tdj <liestidjedam@gmail.com>
2022-02-15 11:16:50 +0100

    Annulation of the modification

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ import express from 'express';
 import path, {dirname} from'path';
 import {fileURLToPath} from 'url';
 
-
 const app = express();
 const port = 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 
 app.get('/', (req, res) => {
     res.sendFile(
